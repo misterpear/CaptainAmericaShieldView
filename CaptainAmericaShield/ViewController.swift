f5295d54c74ec98c74346847d1df367cef5129fa
misterpear <misterpear.dev@gmail.com>
2017-12-19 20:05:43 +0100

    Method for debugging

--- CaptainAmericaShield/ViewController.swift
+++ CaptainAmericaShield/ViewController.swift
@@ -22,6 +22,42 @@ class ViewController: UIViewController {
         view.addSubview(shieldView)
         
         shieldView.center = view.center
+        
+        addDebug()
+    }
+    
+    func addDebug() {
+        let viewWidth = UIScreen.main.bounds.width
+        let viewHeight = UIScreen.main.bounds.height
+        
+        // For Debug.
+        let shieldBackground = UIView(frame: shieldView.frame)
+        shieldBackground.backgroundColor = .yellow
+        view.insertSubview(shieldBackground, belowSubview: shieldView)
+        
+        // Black line.
+        let lineHeight = Int(shieldView.frame.width)
+        let maxLine = 10
+        
+        for n in 0...maxLine {
+            let line1 = UIView(frame: CGRect(x: n * (lineHeight / maxLine), y: 0, width: 1, height: lineHeight))
+            line1.backgroundColor = .black
+            shieldView.addSubview(line1)
+        }
+        
+        // Red line.
+        let lineHorizontal = UIView(frame: CGRect(x: 0, y: 0, width: viewWidth, height: 1))
+        lineHorizontal.backgroundColor = UIColor.cyan
+        
+        view.addSubview(lineHorizontal)
+        
+        let lineVertical = UIView(frame: CGRect(x: 0, y: 0, width: 1, height: viewHeight))
+        lineVertical.backgroundColor = UIColor.cyan
+        
+        view.addSubview(lineVertical)
+        
+        lineHorizontal.center = view.center
+        lineVertical.center = view.center
     }
 
 }
